b157da4319e521f35fa8b8402982512ce7d173d9
Tamer Jb <tamerjb.98@gmail.com>
2022-10-25 22:26:53 +0300

    edited login

--- frontend/src/components/AddPlacePopup.js
+++ frontend/src/components/AddPlacePopup.js
@@ -25,41 +25,41 @@ const AddPlacePopup = ({ isLoading, isOpen, onClose, onAddPlaceSubmit }) => {
 
   return (
     <PopupWithForm
-      title='New place'
-      name='popup-place'
+      title="New place"
+      name="popup-place"
       isOpen={isOpen}
       onClose={onClose}
       buttonText={isLoading ? 'Saving...' : 'Create'}
       onSubmit={handleSubmit}
     >
-      <fieldset className='form__fieldset'>
-        <div className='form__input-container'>
+      <fieldset className="form__fieldset">
+        <div className="form__input-container">
           <input
-            id='place-title-input'
-            type='text'
-            name='name'
-            placeholder='Title'
-            className='form__input form__input_type_place-name'
+            id="place-title-input"
+            type="text"
+            name="name"
+            placeholder="Title"
+            className="form__input form__input_type_place-name"
             required
-            minLength='1'
-            maxLength='30'
+            minLength="1"
+            maxLength="30"
             onChange={handleNameChange}
             value={name}
           />
-          <span className='form__input-error place-title-input-error' />
+          <span className="form__input-error place-title-input-error"></span>
         </div>
-        <div className='form__input-container'>
+        <div className="form__input-container">
           <input
-            id='place-url-input'
-            type='url'
-            name='link'
-            placeholder='Image link'
-            className='form__input form__input_type_place-url'
+            id="place-url-input"
+            type="url"
+            name="link"
+            placeholder="Image link"
+            className="form__input form__input_type_place-url"
             required
             onChange={handleLinkChange}
             value={link}
           />
-          <span className='form__input-error place-url-input-error' />
+          <span className="form__input-error place-url-input-error" />
         </div>
       </fieldset>
     </PopupWithForm>
